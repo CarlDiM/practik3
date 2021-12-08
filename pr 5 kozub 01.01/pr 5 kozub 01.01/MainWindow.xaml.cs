31988f4d34e6366dccdd13b7f3ab592b0755eeb0
CarlDiM <95696540+CarlDiM@users.noreply.github.com>
2021-12-08 08:32:16 +0300

    Add files via upload

---
+++ pr 5 kozub 01.01/pr 5 kozub 01.01/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr_5_kozub_01._01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Man firstHuman = new();
+        private Man secondHuman = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void about_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №5\n" +
+                "Козуб Дмитрий ИСП-34\n" +
+                "Создать класс Man (человек), с полями: имя, возраст, пол и вес. Создать необходимые методы и свойства." +
+                "Создать перегруженные методы SetParams, для установки параметров человека.", "О программе", MessageBoxButton.OK);
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void createFirstHuman_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string name = inputFirstName.Text;
+                int age = Convert.ToInt32(inputFirstAge.Text);
+                char sex = Convert.ToChar(inputFirstSex.Text);
+                int weight = Convert.ToInt32(inputFirstWeight.Text);
+                firstHuman.SetParams(name, sex, age, weight);
+                MessageBox.Show("Человек с заданными параметрами создан", "Успех");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                MessageBox.Show("Возраст не может быть отрицательным", "Ошибка");
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте введенные значения", "Ошибка");
+            }
+        }
+
+        private void createSecondHuman_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string name = inputSecondName.Text;
+                int age = Convert.ToInt32(inputSecondAge.Text);
+                char sex = Convert.ToChar(inputSecondSex.Text);
+                int weight = Convert.ToInt32(inputSecondWeight.Text);
+                secondHuman.SetParams(name, sex, age, weight);
+                MessageBox.Show("Человек с заданными параметрами создан", "Успех");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                MessageBox.Show("Возраст не может быть отрицательным", "Ошибка");
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте введенные значения", "Ошибка");
+            }
+        }
+    }
+}
---
+++ pr 5 kozub 01.01/pr 5 kozub 01.01/Man.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_5_kozub_01._01
+{
+    internal class Man
+    {
+        private string _name;
+        private char _sex;
+        private int _age;
+        private float _weight;
+        public string Name { get { return _name; } set { _name = value; } }
+
+        public char Sex
+        {
+            get { return _sex; }
+            set
+            {
+                if (value == 'М' || value == 'м' || value == 'Ж' || value == 'ж')
+                    _sex = value;
+                else throw new ArgumentException();
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException();
+                else _age = value;
+            }
+        }
+
+
+        public float Weight
+        {
+            get { return _weight; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException();
+                else _weight = value;
+            }
+        }
+
+        public Man()
+        {
+            _name = "Unnamed";
+            _sex = Sex;
+            _age = 1;
+            _weight = 1;
+        }
+
+        public void SetParams(string name)
+        {
+            Name = name;
+        }
+
+        public void SetParams(string name, char sex, int age, float weight)
+        {
+            Name = name;
+            Sex = sex;
+            Age = age;
+            Weight = weight;
+        }
+
+    }
+}
